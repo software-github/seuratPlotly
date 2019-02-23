1917909d8fd28d5173b573d318e5cebb9a196ef3
milescsmith <miles-smith@omrf.org>
2019-02-23 09:24:13 -0600

    updated

--- R/common_functions.R
+++ R/common_functions.R
@@ -1,18 +1,17 @@
 #' @title PrepDf
 #'
-#' @description Helper function to extract dimensional reduction data from a Seurat object
-#' and format it into a data frame for use in plotting.
+#' @description Helper function to extract dimensional reduction data from a single cell-type
+#' (Seurat, SingleCellExperiment) object and format it into a data frame for use in plotting.
 #'
-#' @param object Seurat object
+#' @param object Data object
 #' @param reduction dimensional reduction to extract
 #' @param dim.1 Dimension one.  X-axis data.
 #' @param dim.2 Dimension two.  Y-axis data.
 #' @param dim.3 Dimension three.  Z-axis data.
 #' @param group.by Identity information to use for grouping.
-#'   Permissible values include meta.data column names.
+#'   Permissible values include meta.data/colData column names.
 #'
-#' @import magrittr
-#' @importFrom tibble rownames_to_column
+#' @importFrom magrittr %<>%
 #' @export
 #'
 #' @examples
@@ -20,73 +19,67 @@ PrepDf <- function(object, ...) {
   UseMethod("PrepDf")
 }
 
-# Seurat 2 objects
 #' @rdname PrepDf
-#' @importFrom Seurat GetDimReduction FetchData
-#' @method PrepDf seurat
+#' @importFrom tibble rownames_to_column
+#' @method PrepDf default
 #' @return data.frame
-PrepDf.seurat <- function(object,
-                   reduction,
-                   dim.1 = 1,
-                   dim.2 = 2,
-                   dim.3 = NULL,
-                   group.by = NULL) {
-  df <- GetDimReduction(
-    object = object,
-    reduction.type = reduction,
-    slot = "cell.embeddings"
-  ) %>%
-    as.data.frame()
-
-  dim.code <- GetDimReduction(
-    object = object,
-    reduction.type = reduction,
-    slot = "key"
-  )
-
-  dim.axes <- colnames(
-    GetDimReduction(
-      object = object,
-      reduction.type = reduction,
-      slot = "cell.embeddings"
-    )
-  )
+PrepDF.default <- function(object,
+                           dim.1 = 1,
+                           dim.2 = 2,
+                           dim.3 = NULL,
+                           group.by = NULL) {
 
   if (is.null(dim.3)) {
-    dim.code <- c(
-      dim.axes[[dim.1]],
-      dim.axes[[dim.2]]
-    )
+    df <- df[, c(dim.1, dim.2, ncol(df))]
   } else {
-    dim.code <- c(
-      dim.axes[[dim.1]],
-      dim.axes[[dim.2]],
-      dim.axes[[dim.3]]
-    )
+    df <- df[, c(dim.1, dim.2, dim.3, ncol(df))]
   }
 
-  df <- df[, dim.code]
-  cell_names <- rownames(df)
-
   column.titles <- letters[24:26]
-  colnames(df) <- column.titles[c(
+  colnames(df) <- c(column.titles[c(
     dim.1,
     dim.2,
     dim.3
-  )]
-  rownames(df) <- cell_names
-
-  if (!is.null(group.by)){
-    if (group.by != "ident") {
-      df$ident <- object$group.by %>%
-        as.factor()
-    } else if (group.by == "ident"){
-      df$ident <- Idents(object) %>% as.factor()
-    }
-  }
+  )], "ident")
 
   df %<>% rownames_to_column("cell")
 
+  return(df)
+}
+
+
+# Seurat 2 objects
+#' @rdname PrepDf
+#' @importFrom Seurat GetCellEmbeddings
+#' @method PrepDf seurat
+#' @return data.frame
+PrepDf.seurat <- function(object,
+                   dim.1 = 1,
+                   dim.2 = 2,
+                   dim.3 = NULL,
+                   reduction,
+                   group.by = NULL) {
+  df <- GetCellEmbeddings(
+    object = object,
+    reduction.type = reduction) %>%
+    as.data.frame()
+
+  if (!is.null(group.by)){
+    if (group.by != "ident") {
+      df$ident <- object@meta.data[[group.by]] %>%
+        as.factor()
+    } else if (group.by == "ident"){
+      df$ident <- object@idents %>%
+        as.factor()
+    }
+  }
+
+  df <- PrepDF(df,
+               dim.1,
+               dim.2,
+               dim.3,
+               reduction,
+               group.by)
   return(df)
 }
 
@@ -100,48 +93,62 @@ PrepDf.Seurat <- function(object,
                           dim.1 = 1,
                           dim.2 = 2,
                           dim.3 = NULL,
+                          reduction,
                           group.by = NULL) {
   df <- Embeddings(
     object = object,
     reduction = reduction) %>%
     as.data.frame()
 
-  dim.axes <- colnames(df)
-
-  if (is.null(dim.3)) {
-    dim.code <- c(
-      dim.axes[[dim.1]],
-      dim.axes[[dim.2]]
-    )
-  } else {
-    dim.code <- c(
-      dim.axes[[dim.1]],
-      dim.axes[[dim.2]],
-      dim.axes[[dim.3]]
-    )
-  }
-
-  df <- df[, dim.code]
-  cell_names <- rownames(df)
-  ident <- Idents(object) %>% as.factor()
-
-  column.titles <- letters[24:26]
-  colnames(df) <- column.titles[c(
-    dim.1,
-    dim.2,
-    dim.3
-  )]
-  rownames(df) <- cell_names
-
   if (!is.null(group.by)){
     if (group.by != "ident") {
-      df$ident <- object$group.by %>%
+      df$ident <- object[[group.by]] %>%
         as.factor()
     } else if (group.by == "ident"){
-      df$ident <- Idents(object) %>% as.factor()
+      df$ident <- Idents(object) %>%
+        as.factor()
     }
   }
-  df %<>% rownames_to_column("cell")
+
+  df <- PrepDF(df,
+               dim.1,
+               dim.2,
+               dim.3,
+               reduction,
+               group.by)
+  return(df)
+}
+
+# SingleCellExperiment
+#' @rdname PrepDf
+#' @importFrom SingleCellExperiment reducedDim colData
+#' @method PrepDf SingleCellExperiment
+#' @return data.frame
+PrepDf.SingleCellExperiment <- function(object,
+                                        reduction,
+                                        dim.1 = 1,
+                                        dim.2 = 2,
+                                        dim.3 = NULL,
+                                        reduction,
+                                        group.by = NULL) {
+  df <- reducedDim(x = object,
+                   type = reduction) %>%
+    as.data.frame()
+
+  if (!is.null(group.by)){
+    df$ident <- colData(object)[[group.by]] %>%
+        as.factor()
+  } else {
+    message("No grouping variable provided.  Using a dummy value.")
+    df$ident <- as.factor("none")
+  }
+
+  df <- PrepDF(df,
+               dim.1,
+               dim.2,
+               dim.3,
+               reduction,
+               group.by)
 
   return(df)
 }
@@ -168,19 +175,18 @@ PrepInfo <- function(object, ...) {
   UseMethod("PrepInfo")
 }
 
-# Seurat 2
 #' @rdname PrepInfo
-#' @method PrepInfo seurat
+#' @method PrepInfo default
 #' @return data.frame
-PrepInfo.seurat <- function(object, pt.info, df) {
+PrepInfo.default <- function(object, pt.info, df) {
   if (!is.null(pt.info)) {
     meta.info <- list()
     # for each row
-    for (i in seq(dim(df)[1])) {
+    for (i in seq(nrow(df))) {
       # for each member of pt.info
       rowinfo <- ""
       for (j in 1:length(pt.info)) {
-        rowinfo <- glue("{rowinfo} </br> {pt.info[j]}: {object@meta.data[i, pt.info[j]]}")
+        rowinfo <- glue("{rowinfo} </br> {pt.info[j]}: {object[i, pt.info[j]]}")
       }
       meta.info <- c(meta.info, rowinfo)
     }
@@ -192,10 +198,22 @@ PrepInfo.seurat <- function(object, pt.info, df) {
   return(df)
 }
 
+# Seurat 2
+#' @rdname PrepInfo
+#' @method PrepInfo seurat
+#' @return data.frame
+PrepInfo.seurat <- function(object, pt.info, df) {
+  feature_info <- FetchData(object = object,
+                            vars = pt.info) %>%
+    rownames_to_column("cell")
+  df <- PrepInfo(feature_info, pt.info, df)
+  return(df)
+}
+
 # Seurat 3
 #' @rdname PrepInfo
-#' @method PrepInfo Seurat
-#' @importFrom Seurat FetchData Idents
+#' @method PrepInfo Seurat3
+#' @importFrom Seurat FetchData
 #' @importFrom tibble rownames_to_column
 #' @importFrom dplyr inner_join
 #' @importFrom magrittr %>% %<>%
@@ -204,6 +222,27 @@ PrepInfo.seurat <- function(object, pt.info, df) {
 PrepInfo.Seurat <- function(object, pt.info, df) {
   if (!is.null(pt.info)) {
     feature_info <- FetchData(object = object, vars = pt.info) %>% rownames_to_column("cell")
+
+  } else {
+    df$meta.info <- Idents(object)
+  }
+  return(df)
+}
+
+# SingleCellExperiment
+#' @rdname PrepInfo
+#' @method PrepInfo SingleCellExperiment
+#' @importFrom Seurat FetchData Idents
+#' @importFrom tibble rownames_to_column
+#' @importFrom dplyr inner_join
+#' @importFrom magrittr %>% %<>%
+#' @importFrom glue glue
+#' @return data.frame
+PrepInfo.SingleCellExperiment <- function(object, pt.info, df) {
+  if (!is.null(pt.info)) {
+    feature_info <- colData(object)[which(colnames(colData(object)) %in% pt.info)] %>%
+      as.data.frame() %>%
+      rownames_to_column("cell")
     # for each row
     if ("cell" %nin% colnames(df)){
       df %<>% as.data.frame() %>% rownames_to_column("cell")
@@ -225,10 +264,12 @@ PrepInfo.Seurat <- function(object, pt.info, df) {
     meta.info <- unlist(meta.info)
     df$meta.info <- meta.info
   } else {
-    df$meta.info <- Idents(object)
+    df$meta.info <- df$ident
   }
   return(df)
 }
+
+
 
 #' @title PrepPalette
 #'
